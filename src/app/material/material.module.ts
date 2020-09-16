4dfc141562a489be2938b36688e329f6a24d03ed
Alexander Agyapong <30531859+alexAgyapong@users.noreply.github.com>
2020-09-16 14:25:29 +0100

    Add ngx bootstrap and mat toolbar

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import {MatIconModule  } from '@angular/material/icon';
+import {MatToolbarModule } from '@angular/material/toolbar';
 
 
 const MaterialModules = [
   MatButtonModule,
-  MatIconModule
+  MatIconModule,
+  MatToolbarModule
 ];
 
 
